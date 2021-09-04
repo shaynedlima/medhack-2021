e630c2963fc9b2080b0dbf8ae81501f2f3b61aeb
shaynedlima <dlimashayne@gmail.com>
2021-09-04 15:53:58 +1000

    Microphone update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,55 +1,12 @@
 import React, { useState } from "react";
 import { ReactMic } from "react-mic";
-
-const FFT = require("fft.js");
+import Microphone from "../components/Microphone"
 
 function Home() {
-  const [record, setRecord] = useState(false);
-
-  const startRecording = () => {
-    setRecord(true);
-  };
-
-  const stopRecording = () => {
-    setRecord(false);
-  };
-
-  const onData = (recordedBlob) => {
-    console.log("chunk of real-time data is: ", recordedBlob);
-    let fileReader = new FileReader();
-    let arrayBuffer;
-    fileReader.onloadend = () => {
-      arrayBuffer = fileReader.result;
-      const view = new Uint8Array(arrayBuffer);
-      console.log(view);
-      const f = new FFT(4096);
-      const out = f.createComplexArray();
-      f.realTransform(out, view);
-      console.log(out);
-    };
-    fileReader.readAsArrayBuffer(recordedBlob);
-  };
-
-  const onStop = ({ recordedBlob }) => {
-    console.log("recordedBlob is: ", recordedBlob);
-  };
 
   return (
     <div>
-      <ReactMic
-        record={record}
-        className="sound-wave"
-        onStop={onStop}
-        onData={onData}
-        strokeColor="#000000"
-        backgroundColor="#FF4081"
-      />
-      <button onClick={startRecording} type="button">
-        Start
-      </button>
-      <button onClick={stopRecording} type="button">
-        Stop
-      </button>
+      <Microphone/>
     </div>
   );
 }
