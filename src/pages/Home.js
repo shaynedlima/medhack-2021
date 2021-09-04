a10fd27c1576757b5d5623c34b479080a82bac63
shaynedlima <dlimashayne@gmail.com>
2021-09-04 12:32:21 +1000

    Code backbones

---
+++ src/pages/Home.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { ReactMic } from 'react-mic';
+
+
+function Home() {
+
+    // const [record, setRecord] = useState(false)
+
+    // const startRecording = () => {
+    //     setRecord(true);
+    // }
+
+    // const stopRecording = () => {
+    //     setRecord(false);
+    // }
+
+    // const onData = ({recordedBlob}) => {
+    //     console.log('chunk of real-time data is: ', recordedBlob);
+    // }
+
+    // const onStop = ({recordedBlob}) => {
+    //     console.log('recordedBlob is: ', recordedBlob);
+    // }
+
+    return (
+        <div>
+            {/* <ReactMic
+                record={record}
+                className="sound-wave"
+                onStop={onStop}
+                onData={onData}
+                strokeColor="#000000"
+                backgroundColor="#FF4081" /> */}
+            {/* <button onClick={this.startRecording} type="button">Start</button>
+            <button onClick={this.stopRecording} type="button">Stop</button> */}
+            Testing
+        </div>
+    )
+}
+
+export default Home
